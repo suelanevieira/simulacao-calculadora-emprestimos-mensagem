89a47d491f6e308c4481f6c8510b5d6826790af5
perkles <suelane.vieira@gmail.com>
2022-07-19 15:03:32 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtração(9, 1.8);
+        Calculadora.multiplicação(7, 8);
+        Calculadora.divisão(5, 2.5);
+
+        // Mensagem
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        // Emprestimo
+        System.out.println("Exercicio emprestimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000,5);
+    }
+}
